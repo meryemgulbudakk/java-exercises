5361795cabb32c3bd5d4eba8ab9617676cfac7dd
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-11-27 16:06:20 +0300

    Add product management endpoint

---
+++ src/main/java/com/orderstream/orderprocessor/orders/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.orderstream.orderprocessor.orders.controller;
+
+import com.orderstream.orderprocessor.orders.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/update-stock")
+    public String updateStock(@RequestParam Long productId, @RequestParam int quantity) throws InterruptedException {
+        productService.updateStock(productId, quantity);
+        return "Stok başarıyla güncellendi";
+    }
+}
