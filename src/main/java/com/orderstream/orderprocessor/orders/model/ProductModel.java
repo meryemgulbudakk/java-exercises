c3394e5cd1a5d9c671dbfba9bfba1fddf94b5ae3
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-11-27 16:21:00 +0300

    add entity classes for products

---
+++ src/main/java/com/orderstream/orderprocessor/orders/model/ProductModel.java
@@ -0,0 +1,67 @@
+package com.orderstream.orderprocessor.orders.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "product")
+public class ProductModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrdersModel> orders = new ArrayList<>();
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public List<OrdersModel> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrdersModel> orders) {
+        this.orders = orders;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
