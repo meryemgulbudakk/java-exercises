3b503989aed5bc94d852444d31e640e2edd40fd7
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-11-29 02:40:57 +0300

    add business logic for product

--- src/main/java/com/orderstream/orderprocessor/orders/service/ProductService.java
+++ src/main/java/com/orderstream/orderprocessor/orders/service/ProductService.java
@@ -16,24 +16,24 @@ public class ProductService {
     private final ReentrantLock lock = new ReentrantLock();
 
     public void updateStock(Long productId, int quantity) throws InterruptedException {
-        System.out.println("Stok kontrol başlatıldı: Ürün ID: " + productId);
+        System.out.println("Stock check initiated: Product ID: " + productId);
         if (lock.tryLock(10, TimeUnit.SECONDS)) {
             try {
                 ProductModel productModel = productRepo.findById(productId)
-                        .orElseThrow(() -> new RuntimeException("Ürün bulunamadı: ürün id" + productId));
+                        .orElseThrow(() -> new RuntimeException("Product not found: Product ID" + productId));
                 if (productModel.getQuantity() < quantity) {
-                    throw new RuntimeException("Yeterli stok yok!, ürün id" + productId);
+                    throw new RuntimeException("Insufficient stock!, Product ID" + productId);
                 }
                 productModel.setQuantity(productModel.getQuantity() - quantity);
                 productRepo.save(productModel);
-                System.out.println("Stok başarıyla güncellendi: Ürün ID: " + productId + ", Yeni stok: " + productModel.getQuantity());
+                System.out.println("Stock updated successfully: Product ID: " + productId + ", New stock: " + productModel.getQuantity());
 
             } finally {
                 lock.unlock();
             }
         } else {
-            System.err.println("Stok güncelleme işlemi zaman aşımına uğradı: Ürün ID: " + productId);
-            throw new RuntimeException("Stok güncelleme işlemi zaman aşımına uğradı!");
+            System.err.println("Stock update operation timed out: Product ID: " + productId);
+            throw new RuntimeException("Stock update operation timed out!");
         }
     }
 
