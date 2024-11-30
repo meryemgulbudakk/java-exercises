fa36cdaa9ab4cc2a6223d3259557d4fa207e60d4
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-11-30 05:44:39 +0300

    add JPA repository for customer entity

--- src/main/java/com/orderstream/orderprocessor/orders/repository/CustomerRepository.java
+++ src/main/java/com/orderstream/orderprocessor/orders/repository/CustomerRepository.java
@@ -1,7 +1,7 @@
 package com.orderstream.orderprocessor.orders.repository;
 
-import com.orderstream.orderprocessor.orders.model.CustomerModel;
+import com.orderstream.orderprocessor.orders.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<CustomerModel, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
