b0f6da6704a0f05e0b30351df0b306a9bb333cfc
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-11-30 05:45:59 +0300

    add JPA repository for product entity

--- src/main/java/com/orderstream/orderprocessor/orders/repository/ProductRepository.java
+++ src/main/java/com/orderstream/orderprocessor/orders/repository/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.orderstream.orderprocessor.orders.repository;
 
-import com.orderstream.orderprocessor.orders.model.ProductModel;
+import com.orderstream.orderprocessor.orders.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
