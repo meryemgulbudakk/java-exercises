108f1dc91fe4eaf90e14b1dafbd001c33d9d601c
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2025-01-20 20:15:48 +0300

    warmup

---
+++ hackerRank/MiniMaxSum.java
@@ -0,0 +1,19 @@
+package hackerRank;
+
+import java.util.List;
+
+public class MiniMaxSum {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int leftToRight = 0;
+        int rightToLeft = 0;
+        int counter = arr.size() - 1;
+        for (int i = 0; i < arr.size(); i++) {
+            leftToRight += arr.get(i).get(i);
+            rightToLeft += arr.get(i).get(counter);
+            counter--;
+        }
+
+        int result = Math.abs((leftToRight - rightToLeft));
+        return result;
+    }
+}
